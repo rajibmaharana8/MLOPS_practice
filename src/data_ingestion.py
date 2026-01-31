1662314e0b360182c48bfda05517224550d49b29
Rajib Kumar Maharana <rajibmaharana8200@gmail.com>
2026-01-31 19:07:28 +0530

    Parents configured for the pipeline

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -85,9 +85,10 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        #params = load_params(params_path='params.yaml')
-        #test_size = params['data_ingestion']['test_size']
-        test_size = 0.2
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
+        #test_size = 0.2
+        
         data_path = 'https://raw.githubusercontent.com/vikashishere/YT-MLOPS-Complete-ML-Pipeline/refs/heads/main/experiments/spam.csv'
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
